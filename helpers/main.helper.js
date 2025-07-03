40a16846e852abb1e11d21ea2404bb1590b810e7
Siddhartht Twari <siddharthtiwari0308@gmail.com>
2025-07-03 12:31:51 +0530

    default else handles null values gracefully

--- helpers/main.helper.js
+++ helpers/main.helper.js
@@ -432,9 +432,7 @@ const prepCase = ({ alias, val, junction = 'and', values, encryption = undefined
         const thenPlaceholder = then == null || then == 'null' ? null : handlePlaceholder({ alias, value: then, junction, values, encryption, ctx })
         return `WHEN ${whenPlaceholder} THEN ${thenPlaceholder}`
     })
-
-    const elsePlaceholder = prepPlaceholder({ value: defaultElse, alias, ctx })
-    if (isVariable(elsePlaceholder)) values.push(prepName({ alias, value: defaultElse, ctx }))
+    const elsePlaceholder = defaultElse == null || defaultElse == 'null' ? null : handlePlaceholder({ alias, value: defaultElse, ctx })
 
     if (as && ctx?.isMySQL) values.push(as)
     if (cast) return `CAST(CASE ${conditionalPlaceholders.join(' ')} ELSE ${elsePlaceholder} END AS ${dataTypes[cast] || 'CHAR'})${as ? ` AS ${ctx?.isMySQL ? '?' : `"${as}"`}` : ''}`
